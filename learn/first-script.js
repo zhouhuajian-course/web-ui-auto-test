55597a08838dd1ca7b477e1fea019f97527c23c6
zhouhuajian <zhouhuajian@company.com>
2024-11-13 10:38:57 +0800

    03 第一个 Playwright 脚本

---
+++ learn/first-script.js
@@ -0,0 +1,13 @@
+const {chromium,firefox,webkit} = require("playwright");
+
+(async () => {
+  // 启动浏览器 默认无头模式 headless 
+  const browser = await firefox.launch({headless: false})
+  const page = await browser.newPage()
+  await page.goto("http://localhost/welcome")
+  const title = await page.title()
+  const content = await page.content()
+  console.log(title);
+  console.log(content);
+  await browser.close()
+})()
