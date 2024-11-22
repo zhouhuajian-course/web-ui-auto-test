acddf299db45c51c4731e1a4320d4e7d63b4d37c
zhouhuajian <zhouhuajian@company.com>
2024-11-22 10:18:57 +0800

    04 使用 Codegen 录制代码

--- learn/first-script.js
+++ learn/first-script.js
@@ -1,8 +1,8 @@
-const {chromium,firefox,webkit} = require("playwright");
+const { chromium, firefox, webkit } = require("playwright");
 
 (async () => {
   // 启动浏览器 默认无头模式 headless 
-  const browser = await firefox.launch({headless: false})
+  const browser = await firefox.launch({ headless: false })
   const page = await browser.newPage()
   await page.goto("http://localhost/welcome")
   const title = await page.title()
