de936412510dfa8b6f6d2225defbd21a809f4658
zhouhuajian <zhouhuajian@company.com>
2024-12-02 16:50:24 +0800

    修改登录页面测试用例

--- tests/login-page.spec.js
+++ tests/login-page.spec.js
@@ -1,15 +1,36 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('测试页面基础', async ({ page }) => {
-  await page.goto('http://localhost/login');
-  await expect(page).toHaveTitle("用户登录");
+// test('测试页面基础', async ({ page }) => {
+//   await page.goto('http://localhost/login');
+//   await expect(page).toHaveTitle("用户登录");
+// });
+
+// test('测试登录成功', async ({ page }) => {
+//   await page.goto('http://localhost/login');
+//   await page.locator('input[name="username"]').fill('zhouhuajian');
+//   await page.locator('input[name="password"]').fill('zhouhuajian');
+//   await page.getByRole('button', { name: '登录' }).click();
+//   await expect(page.getByText('zhouhuajian, 欢迎回来！')).toBeVisible();
+// });
+
+// @ts-check
+// const { test, expect } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('测试登录成功', async ({ page }) => {
-  await page.goto('http://localhost/login');
-  await page.locator('input[name="username"]').fill('zhouhuajian');
-  await page.locator('input[name="password"]').fill('zhouhuajian');
-  await page.getByRole('button', { name: '登录' }).click();
-  await expect(page.getByText('zhouhuajian, 欢迎回来！')).toBeVisible();
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
